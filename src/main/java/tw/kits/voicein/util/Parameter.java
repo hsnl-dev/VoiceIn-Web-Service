deabc8c71fa7ae01aa26daf11838c12c00dbbef6
Henry Chang <qoo392@hotmail.com>
2016-02-25 21:22:37 +0800

    finish avatar upload

--- src/main/java/tw/kits/voicein/util/Parameter.java
+++ src/main/java/tw/kits/voicein/util/Parameter.java
@@ -7,5 +7,5 @@ public interface Parameter {
     public static final String API_ROOT = "https://ts.kits.tw/projectLYS/";
     public static final String API_VER = "v0/";
     public static final String API_KEY = "2a4e0dd8db3807790d853dabf0f448de21cea6057b5dc48539330f934e9bddfb";
-    public static final AWSCredentials AWS_CREDENTIALS = new BasicAWSCredentials("AKIAJ36VH2C3IRLYIXKA", "MDXyJZhLXIwjrXjLa37keevLwRJ5Zxtci6P6rwWY");
+    public static final AWSCredentials AWS_CREDENTIALS = new BasicAWSCredentials("AKIAI4RNBMGTLTP6HWFQ", "bKOJ+9emilF2cZIK+44bRBoRHpPxyJmfc7y+VpFe");
 }
