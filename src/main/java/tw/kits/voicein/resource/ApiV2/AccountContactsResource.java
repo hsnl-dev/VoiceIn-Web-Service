612f02219bdb1b99005d6639f40e7c30f79496b9
Henry <qoo392@hotmail.com>
2016-03-22 22:03:15 +0800

    fix del icon from contact problem

--- src/main/java/tw/kits/voicein/resource/ApiV2/AccountContactsResource.java
+++ src/main/java/tw/kits/voicein/resource/ApiV2/AccountContactsResource.java
@@ -24,6 +24,7 @@ import tw.kits.voicein.bean.UserContactBean;
 import tw.kits.voicein.model.Contact;
 import tw.kits.voicein.model.Icon;
 import tw.kits.voicein.model.User;
+import tw.kits.voicein.util.ContactConstants;
 import tw.kits.voicein.util.Helpers;
 import tw.kits.voicein.util.MongoManager;
 import tw.kits.voicein.util.TokenRequired;
@@ -201,10 +202,16 @@ public class AccountContactsResource {
  
         User provider = payContact.getProviderUser();
         String qrCodeUuid = payContact.getQrCodeUuid();
-        Contact freeContact = dataStoreObject.createQuery(Contact.class).filter("qrCodeUuid =", qrCodeUuid).filter("user =", provider).get();
+        if(payContact.getChargeType()!=ContactConstants.TYPE_ICON){
+            Contact freeContact = dataStoreObject.createQuery(Contact.class).filter("qrCodeUuid =", qrCodeUuid).filter("user =", provider).get();
+            dataStoreObject.delete(freeContact);
+        }else {
+            dataStoreObject.delete(payContact.getCustomerIcon());
+        }
+       
 
         dataStoreObject.delete(payContact);
-        dataStoreObject.delete(freeContact);
+        
         return Response.ok().build();
     }
 }
