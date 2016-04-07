2167e72311d82b40ea61869cb08018ce9ad39706
lockys <locky4567@gmail.com>
2016-04-07 19:56:32 +0800

    group - post/put method

--- src/main/java/tw/kits/voicein/resource/ApiV1/AccountGroupsResource.java
+++ src/main/java/tw/kits/voicein/resource/ApiV1/AccountGroupsResource.java
@@ -1,5 +1,8 @@
 package tw.kits.voicein.resource.ApiV1;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Logger;
 import javax.validation.Valid;
@@ -16,7 +19,11 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+import org.bson.types.ObjectId;
 import org.mongodb.morphia.Datastore;
+import tw.kits.voicein.bean.AccountAllGroupBean;
+import tw.kits.voicein.bean.AccountGroupUpdateBean;
+import tw.kits.voicein.model.Contact;
 import tw.kits.voicein.model.Group;
 import tw.kits.voicein.model.User;
 import tw.kits.voicein.util.MongoManager;
@@ -47,17 +54,40 @@ public class AccountGroupsResource {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @TokenRequired
+    @SuppressWarnings("empty-statement")
     public Response getGroupList(
             @PathParam("uuid") String uuid
     ) {
         User owner = dataStoreObject.get(User.class, uuid);
-        return Response.ok().build();
+        List<Group> groups = dataStoreObject.createQuery(Group.class).field("user").equal(owner).asList();
+        ArrayList<HashMap<String, Object>> groupsEntities = new ArrayList();
+        AccountAllGroupBean accountAllGroupBean = new AccountAllGroupBean();
+        
+        for (Group group : groups) {
+            ArrayList<String> contactsId = group.getContacts();
+            ArrayList<Contact> contactEntities = new ArrayList();
+            HashMap<String, Object> groupEntity = new HashMap();
+            
+            groupEntity.put("groupName", group.getGroupName());
+            
+            for (String contactId : contactsId) {
+                Contact contact = dataStoreObject.get(Contact.class, new ObjectId(contactId));
+                contactEntities.add(contact);
+            }
+            groupEntity.put("contacts", contactEntities);
+            groupsEntities.add(groupEntity);
+        }
+        
+        accountAllGroupBean.setGroups(groupsEntities);
+        return Response.ok(accountAllGroupBean).build();
     }
 
     /**
      *
      * @param uuid
+     * @param groupUuid
      * @param action - expected parameters are delete or add.
+     * @param contactsToUpdate
      * @return
      */
     @PUT
@@ -67,10 +97,44 @@ public class AccountGroupsResource {
     @TokenRequired
     public Response updateContactInFGroup(
             @PathParam("uuid") String uuid,
-            @QueryParam("action") String action
+            @PathParam("groupUuid") String groupUuid,
+            @QueryParam("action") String action,
+            AccountGroupUpdateBean contactsToUpdate
     ) {
+        ArrayList<String> contactsToModified = contactsToUpdate.getContacts();
+        Group group = dataStoreObject.get(Group.class, groupUuid);
+        ArrayList<String> existedContacts = group.getContacts();
 
-        return Response.ok().build();
+        if (group != null) {
+            // The group is found!
+            switch (action) {
+                case "add":
+
+                    for (String contact : contactsToModified) {
+                        existedContacts.add(contact);
+                    }
+
+                    group.setContacts(existedContacts);
+                    dataStoreObject.save(group);
+
+                    return Response.ok().build();
+                case "delete":
+                    for (String contact : contactsToModified) {
+                        existedContacts.remove(contact);
+                    }
+
+                    group.setContacts(existedContacts);
+                    dataStoreObject.save(group);
+
+                    return Response.ok().build();
+                default:
+                    return Response.status(Status.NOT_ACCEPTABLE).build();
+
+            }
+        } else {
+            // The aimed group is not found.
+            return Response.status(Status.NOT_FOUND).build();
+        }
     }
 
     /**
@@ -113,7 +177,7 @@ public class AccountGroupsResource {
             @PathParam("groupUuid") String groupUuid
     ) {
         Group groupToDelete = dataStoreObject.get(Group.class, groupUuid);
-        
+
         if (groupToDelete != null) {
             dataStoreObject.delete(groupToDelete);
             return Response.ok().build();
